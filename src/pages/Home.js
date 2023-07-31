8e813cd4ebcd9f42c847b10866be398f1a6ae83d
RedBoard <ott.thomas68@gmail.com>
2023-07-31 11:49:16 +0200

    (|) fix respnsivity of the profileImage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,26 @@
-import React from "react";
-import './Home.css';
+import React, { } from 'react';
 import ProfileImage from '../components/ProfileImage';
 import Button from '@mui/material/Button';
+import "./Home.css";
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div className="background">
-                <div className="profile-title-container">
-                    <ProfileImage />
-                    <h1 className="title">Thomas OTT</h1>
-                    <h2 className="construction">Site web en construction</h2>
-                    <div className="button-container">
-                        <Button variant="outlined" color="primary" href="https://www.linkedin.com/in/thomas--ott/" target="_blank" rel="noopener noreferrer">
-                            Linkedin
-                        </Button>
-                        <Button variant="outlined" color="primary" href="/works">
-                            My Works
-                        </Button>
-                    </div>
+function Home() {
+    return (
+        <div className="home">
+            <div className="profile-title-container">
+                <ProfileImage />
+                <h1 className="title">Thomas OTT</h1>
+                <h2 className="construction">Site web en construction</h2>
+                <div className="button-container">
+                    <Button variant="outlined" color="primary" href="https://www.linkedin.com/in/thomas--ott/" target="_blank" rel="noopener noreferrer">
+                        Linkedin
+                    </Button>
+                    <Button variant="outlined" color="primary" href="/works">
+                        My Works
+                    </Button>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Home;
