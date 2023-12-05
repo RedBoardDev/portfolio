37392ceacf94e6e325e3937057905285efa6aef7
RedBoardDev <ott.thomas68@gmail.com>
2023-12-05 12:20:08 +0100

    [+] connect 'Recent work' cards with github API

--- src/components/ProjectCardSection.js
+++ src/components/ProjectCardSection.js
@@ -1,54 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import './ProjectCardSection.css';
 import ProjectCard from '../components/ProjectCard';
 
-import project1Img from '../assets/projects/project1.png';
-import project2Img from '../assets/projects/project2.png';
-import project3Img from '../assets/projects/project3.png';
-import project4Img from '../assets/projects/project4.png';
-import project5Img from '../assets/projects/project5.png';
-import project6Img from '../assets/projects/project6.png';
-
 function ProjectCardSection({ number }) {
-    const projects = [
-        {
-            projectName: "Projet 1",
-            projectDesc: "Description du projet 1",
-            projectImg: project1Img,
-            projectLink: "google.com"
-        },
-        {
-            projectName: "Projet 2",
-            projectDesc: "Description du projet 2",
-            projectImg: project2Img,
-            projectLink: "google.com"
-        },
-        {
-            projectName: "Projet 3",
-            projectDesc: "Description du projet 3",
-            projectImg: project3Img,
-            projectLink: "google.com"
-        },
-        {
-            projectName: "Projet 4",
-            projectDesc: "Description du projet 4",
-            projectImg: project4Img,
-            projectLink: "google.com"
-        },
-        {
-            projectName: "Projet 5",
-            projectDesc: "Description du projet 5",
-            projectImg: project5Img,
-            projectLink: "google.com"
-        },
-        {
-            projectName: "Projet 6",
-            projectDesc: "Description du projet 6",
-            projectImg: project6Img,
-            projectLink: "google.com"
-        }
-    ];
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+
+        const fetchGitHubProjects = async () => {
+            try {
+                const response = await fetch('https://api.github.com/users/redboarddev/repos?sort=pushed&direction=desc');
+                if (!response.ok) {
+                    throw new Error('Error while retrieving GitHub data');
+                }
+
+                const data = await response.json();
+
+                const filteredProjects = data.filter((project) => project.description);
+
+                const slicedProjects = filteredProjects.slice(0, 6);
+
+                const mappedProjects = slicedProjects.map((project) => ({
+                    projectName: project.name,
+                    projectDesc: project.description,
+                    projectImg: project.owner.avatar_url,
+                    projectLink: project.html_url,
+                }));
+
+                setProjects(mappedProjects);
+            } catch (error) {
+                console.error('Erreur:', error);
+            }
+        };
+
+        fetchGitHubProjects();
+    }, []);
 
     return (
         <div className="project-cards-section">
